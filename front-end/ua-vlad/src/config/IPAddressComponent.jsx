34663424d296064e15b093d3dc9b429a04dcec98
Vladyslav <vladislav@ua-vlad.com>
2024-11-03 16:57:01 +0100

    ip

--- front-end/ua-vlad/src/config/IPAddressComponent.jsx
+++ front-end/ua-vlad/src/config/IPAddressComponent.jsx
@@ -22,7 +22,7 @@ const IPAddressComponent = ({ setResponseData }) => {
         console.log(`${import.meta.env.VITE_IP_BACKEND}/api/user-information/`);
         console.log(responceData);
         console.log("test");
-        const postResponse = await axios.post("http://10.8.0.10:8084/api/user-information/", responceData);
+        const postResponse = await axios.post(`${import.meta.env.VITE_IP_BACKEND}/api/user-information/`, responceData);
         console.log("test after");
         console.log(postResponse);
         setResponseData(postResponse.data);
