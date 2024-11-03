796fc620d98ce1db2323c30d6ecb8376306b9a3a
Vladyslav <vladislav@ua-vlad.com>
2024-11-03 11:19:56 +0100

    ip

--- front-end/ua-vlad/src/config/IPAddressComponent.jsx
+++ front-end/ua-vlad/src/config/IPAddressComponent.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import axios from 'axios';
 
 const IPAddressComponent = ({ setResponseData }) => {
+  console.log(`${import.meta.env.VITE_IPINFO_API_KEY}`)
   useEffect(() => {
     const fetchIP = async () => {
       try {
@@ -11,15 +12,16 @@ const IPAddressComponent = ({ setResponseData }) => {
         const userAgent = navigator.userAgent;
         const platform = navigator.platform;
         const uniqueBrowserIdentifier = `${userAgent}-${userLanguage}-${platform}`;
-        console.log(`${import.meta.env.VITE_IP_BACKEND}`)
+        console.log(`${import.meta.env.VITE_IP_BACKEND}/api/user-information/`)
         
-        const postResponse = await axios.post(`http://localhost:8084/api/user-information/`, {
+        const postResponse = await axios.post(`${import.meta.env.VITE_IP_BACKEND}/api/user-information/`, {
           ip_address: ipAddress,
           browser_language: userLanguage,
           user_agent: userAgent,
           platform: platform,
           unique_identifier: uniqueBrowserIdentifier,
         });
+        console.log(postResponse)
 
         setResponseData(postResponse.data);
       } catch (error) {
