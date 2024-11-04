ff6063b09315f8879f645ac6689b896d9192a590
Vladyslav <vladislav@ua-vlad.com>
2024-11-04 19:28:05 +0100

    ip

--- front-end/ua-vlad/src/components/main/components/Home/components/Contacts/Team.jsx
+++ front-end/ua-vlad/src/components/main/components/Home/components/Contacts/Team.jsx
@@ -41,7 +41,7 @@ export default function Team({ team }) {
           <div key={index} className="user-card">
             <section className="user-content">
               <img
-                src={`${userImage}`}
+                src="/team_photos/team_photo.jpg"
                 alt={`${userName} фото`}
                 className="user-photo"
               />
